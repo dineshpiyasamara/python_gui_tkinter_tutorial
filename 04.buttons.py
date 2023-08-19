d8b1a8fcacae68e52b9a85f9634a57362cda8fe7
Dinesh <silvershine.zero@gmail.com>
2023-08-19 14:31:12 +0530

    update buttins

--- 04.buttons.py
+++ 04.buttons.py
@@ -6,21 +6,20 @@ root = tk.Tk()
 root.title('Buttons')
 root.geometry('300x200')
 
-my_variable = tk.StringVar(value='Click Me')
 number1 = tk.IntVar()
 number2 = tk.IntVar()
 sum_of_numbers = tk.StringVar()
 
 def button_func(input1, input2):
     sum = input1.get() + input2.get()
-    sum_of_numbers.set(sum)
+    sum_of_numbers.set(f'Answer is {sum}')
 
 get_number1 = ttk.Entry(root, textvariable=number1)
 get_number2 = ttk.Entry(root, textvariable=number2)
 get_number1.pack()
 get_number2.pack()
 
-button = ttk.Button(root, command=lambda: button_func(number1, number2), textvariable=my_variable)
+button = ttk.Button(root, text="Click Me", command=lambda: button_func(number1, number2))
 button.pack()
 
 label = ttk.Label(root, textvariable=sum_of_numbers)
