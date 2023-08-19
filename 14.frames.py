5011debb81fc947af5d4354defdd9ad2e4b822d2
Dinesh <silvershine.zero@gmail.com>
2023-08-19 17:50:55 +0530

    update frame

--- 14.frames.py
+++ 14.frames.py
@@ -6,30 +6,6 @@ root.title("Frames")
 root.geometry("500x400")
 
 
-####################
-# frame = ttk.Frame(root, width=200, height=100, relief=tk.GROOVE)
-# frame.pack()
-
-####################
-# frame = ttk.Frame(root, width=200, height=100, relief=tk.GROOVE)
-# frame.pack_propagate(False)
-# frame.pack()
-
-# label1 = ttk.Label(frame, text="Hello")
-# label1.pack()
-
-####################
-# frame = ttk.Frame(root, width=200, height=100, relief=tk.GROOVE)
-# frame.pack_propagate(False)
-# frame.pack()
-
-# label1 = ttk.Label(frame, text="Hello")
-# label1.pack()
-
-# label2 = ttk.Label(root, text="Welcome")
-# label2.pack()
-
-####################
 frame = ttk.Frame(root, width=200, height=100, relief=tk.GROOVE)
 frame.pack_propagate(False)
 frame.pack(side='left')
@@ -40,8 +16,14 @@ entry.pack(pady=10)
 button = ttk.Button(frame, text='Submit')
 button.pack()
 
-label2 = ttk.Label(root, text="Welcome")
-label2.pack()
+frame2 = ttk.Frame(root, width=200, height=100, relief=tk.GROOVE)
+frame2.pack(side='bottom')
+
+label2 = ttk.Label(frame2, text="Welcome")
+label2.pack(pady=40)
+
+button2 = ttk.Button(frame2, text='Submit')
+button2.pack(pady=10, padx=10)
 
 
 root.mainloop()
